587c3f9f958e933e5d84ed945c2cc6e0c54a246e
hectoraria <145675493+hectoraria@users.noreply.github.com>
2024-12-03 14:33:21 +0100

    Api Delete Terminado

--- ListadoBDTASP/Controllers/API/PersonaController.cs
+++ ListadoBDTASP/Controllers/API/PersonaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DALD;
+using ENT;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,34 +12,144 @@ namespace ListadoBDTASP.Controllers.API
     {
         // GET: api/<PersonaController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            IActionResult salida;
+            try
+            {
+                List<ClsPersona> listadoCompleto = ListadosDAL.ListadoCompletoPersonasDAL();
+                if (listadoCompleto.Count() == 0)
+                {
+                    salida = NoContent();
+                }
+                else
+                {
+                    salida = Ok(listadoCompleto);
+                }
+            }
+            catch
+            {
+                salida = BadRequest();
+            }
+            return salida;
+
         }
+
 
         // GET api/<PersonaController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            IActionResult salida;
+            try
+            {
+                ClsPersona persona = ClsManejadora.obtenerPersonaPorID(id);
+                if (persona.Nombre == "")
+                {
+                    salida = NoContent();
+                }
+                else
+                {
+                    salida = Ok(persona);
+                }
+            }
+            catch
+            {
+                salida = BadRequest();
+            }
+            return salida;
         }
 
         // POST api/<PersonaController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] ClsPersona persona)
         {
+            int filasAfectadas;
+            IActionResult salida;
+            if (persona.Nombre == "")
+            {
+                salida = BadRequest();
+            }
+            else
+            {
+                try
+                {
+                    filasAfectadas = ClsManejadora.insertarPersonaDAL(persona);
+                    if (filasAfectadas == 0)
+                    {
+                        salida = BadRequest();
+                    }
+                    else
+                    {
+                        salida = Ok(filasAfectadas);
+                    }
+                }
+                catch
+                {
+                    salida = BadRequest();
+                }
+            }
+            return salida;
         }
 
         // PUT api/<PersonaController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(ClsPersona persona)
         {
+            int filasAfectadas;
+            IActionResult salida;
+            if (persona.Nombre == "")
+            {
+                salida = BadRequest();
+            }
+            else
+            {
+                try
+                {
+                    filasAfectadas = ClsManejadora.editarPersonaDAL(persona);
+                    if (filasAfectadas == 0)
+                    {
+                        salida = BadRequest();
+                    }
+                    else
+                    {
+                        salida = Ok(filasAfectadas);
+                    }
+                }
+                catch
+                {
+                    salida = BadRequest();
+                }
+            }
+            return salida;
         }
 
         // DELETE api/<PersonaController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            IActionResult salida;
+            int numFilasAfectadas;
+
+            try
+            {
+                numFilasAfectadas = ClsManejadora.eliminarPersona(id);
+                if (numFilasAfectadas == 0)
+                {
+                    salida = NotFound();
+                }
+                else
+                {
+                    salida = Ok(numFilasAfectadas);
+                }
+            }
+            catch (Exception e)
+            {
+                salida = BadRequest();
+            }
+
+            return salida;
+
         }
     }
 }
